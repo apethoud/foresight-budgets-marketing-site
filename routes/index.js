6d55354d6e9ac675cc3300844fc7412e5e75bc8b
David Ramirez <davidsramirez11@gmail.com>
2017-08-25 23:11:42 -0700

    hide api key

--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,18 @@ router.get('/', function(req, res, next) {
 /* POST notify page. */
 router.post('/notify-me', function(req, res, next) {
 	var user = req.body.email
+	var texts = 'Thank you for subscribing! We\'ll keep you informed about future releases and features of Foresight Budgets.If you\'d like to unsubscribe from this mailing list, simply reply to this email with the word \"Unsubscribe\" in the subject line.'
+	console.log(texts)
 	var helper = require('sendgrid').mail;
 	var fromEmail = new helper.Email('foresightbudgets@gmail.com');
 	var toEmail = new helper.Email(user);
 	var subject = 'Foresight Budgets';
-	var content = new helper.Content('text/plain', 'Thank you for subscribing! We\'ll keep you informed about future releases and features of Foresight Budgets.If you\'d like to unsubscribe from this mailing list, simply reply to this email with the word \"Unsubscribe" in the subject line.');
+	var content = new helper.Content('text/plain', texts);
 	var mail = new helper.Mail(fromEmail, subject, toEmail, content);
 
-	var sg = require('sendgrid')('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');
+	console.log(texts)
+
+	var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
 	var request = sg.emptyRequest({
 	  method: 'POST',
 	  path: '/v3/mail/send',
